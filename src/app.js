7f69dcf598cd0ce0a143dea1e0774b0191f31477
JUAN DAVID LOAIZA REYES <juan400reyesloaiza@gmail.com>
2021-03-25 16:00:01 -0500

    Routes tested

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 //routes
 
 app.get('/', (req, res) => {
-  res.status(200).send("Api Create Micro Services!");
+  res.status(200).json({message: 'Api Create Micro Services!'});
 });
 
 module.exports = app;
