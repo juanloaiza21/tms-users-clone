34a22c8a57313a83049d35082ccf71757bb49a68
JUAN DAVID LOAIZA REYES <juan400reyesloaiza@gmail.com>
2021-03-26 16:45:16 -0500

    refactoring

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,23 +3,6 @@ const router = express.Router();
 
 const User = require('../models/models');
 
-router.post("/create", async (req, res) => {
-	try {
-        const post = new User({
-        Email: req.body.email,
-        NIT: req.body.nit,
-        Nombre: req.body.Nombre,
-        Direccion: req.body.direccion,
-        Tel: req.body.tel,
-        kinduser: req.body.tu,
-        })
-        await post.save()
-        res.status(200).send({message: "Usuario creado"})       
-        } catch (error) {
-        console.log(error);
-        res.status(401).json("Error")
-        }
-});
 
 router.post('/login',(req, res) => {
         //este daniel
@@ -27,8 +10,16 @@ router.post('/login',(req, res) => {
         let password = req.password 
 });
 
-router.post('/preregister',(req, res) => {
-        // este es el que debe hacer juan
+router.post('/preregister',async (req, res) => {
+        try {
+        const {email, nit, Nombre, direccion, tel, tu}= req.body
+        const post = new User({ email, nit, Nombre, direccion, tel, tu})
+        await post.save()
+        res.status(200).send({message: "Usuario creado"})       
+        } catch (error) {
+        console.log(error);
+        res.status(401).json("Error")
+        }
 });
 
 router.post('/resetpassword',(req, res) => {
