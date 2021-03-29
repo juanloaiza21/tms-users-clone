f9bc1275a1ffa44e4f9c5e0f2d3ffd336d7860be
Jorge Ivan Reyes Valencia <jorgeivanreyesvalencia92@gmail.com>
2021-03-29 12:01:07 -0500

    dev

---
+++ src/middlewares/responses.js
@@ -0,0 +1,29 @@
+responseData = (req, res) => {
+    if (req.objects.info.status === 200) {
+        res.json({
+            info: {
+                status: 200,
+                urlServices: req.headers.host,
+                path: req.originalUrl,
+                message: req.objects.info.message
+            },
+            result: req.objects.data
+        });
+    }
+    else {
+        res.status(req.objects.info.status).json({
+            info: {
+                status: req.objects.info.status,
+                urlServices: req.headers.host,
+                path: req.originalUrl,
+            },
+            errors: {
+                descriptions: req.objects.errors
+            }
+        });
+    }
+}
+
+module.exports = {
+    responseData
+}
---
+++ src/middlewares/validatedata.js
@@ -0,0 +1,34 @@
+const {validationResult} = require('express-validator');
+
+function validationData(req, res, next) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+    next();
+}
+
+function formatData(req, res, next) {
+    req.objects = {};
+    req.objects.info = {}
+    switch (req.method) {
+        case 'POST':
+            req.objects.data = req.body
+            
+            break;
+        case 'GET':
+            req.objects.data = req.query
+            break;
+        default:
+            break;
+    }
+    next();
+}
+
+module.exports = {
+    validator: validationData,
+    format: formatData
+}
