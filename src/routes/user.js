f3fe77ebf8aecf164b2e6100f696e2e3cb8db5b5
api create <apicreatecol@gmail.com>
2021-03-29 16:18:57 +0000

    Update src/routes/user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,24 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const controllers =  require('../controllers/controllers')
-
-router.post('/login',(req, res) => {
-        //este daniel
-        let user =  req.user
-        let password = req.password 
-});
-
-router.post('/preregister',async (req, res) => {
-        try {
-        const {email, nit, Nombre, direccion, tel, tu}= req.body
-        const post = new User({ email, nit, Nombre, direccion, tel, tu})
-        await post.save()
-        res.status(200).send({message: "Usuario creado"})       
-        } catch (error) {
-        console.log(error);
-        res.status(401).json("Error")
-        }
-});
 
 router.post('/resetpassword',(req, res) => {
         //este daniel
