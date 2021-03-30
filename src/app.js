146bf42e69a95cf05d3fbda8a91fbcba7d44a33b
Jorge Ivan Reyes Valencia <jorgeivanreyesvalencia92@gmail.com>
2021-03-30 00:52:54 -0500

    refactor

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load('./docs/swagger.yml');
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -10,6 +12,7 @@ app.use(express.urlencoded({
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 
