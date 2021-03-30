145bcc85c89d5fcacd9095af5fffb98fdc92e1d3
Jorge Ivan Reyes Valencia <jorgeivanreyesvalencia92@gmail.com>
2021-03-29 22:42:32 -0500

    refactor

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,7 @@ const controllers =  require('../controllers/controllers')
  
 router.post('/register',controllers.registerUser);
 // router.post('/preregister',controllers.preregister);
-// router.post('/recovery',controllers.recoveryPassword);
+router.post('/recovery',controllers.recoveryPassword);
 // router.post('/login',controllers.login);
 
 module.exports = router;
